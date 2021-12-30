9a7a23ab1181fce26e3890fb086c126d6d20f758
macsko <maskskoczen@gmail.com>
2021-12-30 13:08:37 +0100

    Add set methods and tests

---
+++ ds/set.go
@@ -0,0 +1,116 @@
+package ds
+
+import "fmt"
+
+type Set[K comparable] map[K]struct{} // empty struct use 0 bytes of memory
+
+func MakeSet[K comparable](elems ...K) Set[K] {
+  set := make(Set[K])
+  for _, e := range elems {
+    set[e] = struct{}{}
+  }
+  return set
+}
+
+func (set Set[K]) ToSlice() []K {
+  res := make([]K, 0, set.Size())
+  for e := range set {
+    res = append(res, e)
+  }
+  return res
+}
+
+func (set Set[K]) String() string {
+  str := "set"
+  slice := set.ToSlice()
+  str += fmt.Sprint(slice)
+  return str
+}
+
+func (set Set[K]) Has(k K) bool {
+  _, ok := set[k]
+  return ok
+}
+
+func (set Set[K]) Add(elems ...K) {
+  for _, e := range elems {
+    set[e] = struct{}{}
+  }
+}
+
+func (set Set[K]) Size() int {
+  return len(set)
+}
+
+func (set Set[K]) Delete(k K) {
+  delete(set, k)
+}
+
+func (set Set[K]) Clear() {
+  for e := range set { // optimized by compiler since go1.11
+    delete(set, e)
+  }
+}
+
+func (set Set[K]) Copy() Set[K] {
+  res := make(Set[K])
+  for e := range set {
+    res.Add(e)
+  }
+  return res
+}
+
+func (set1 Set[K]) Equal(set2 Set[K]) bool {
+  if set1.Size() != set2.Size() {
+    return false
+  }
+
+  for e := range set1 {
+    if !set2.Has(e) {
+      return false
+    }
+  }
+
+  for e := range set2 {
+    if !set1.Has(e) {
+      return false
+    }
+  }
+  return true
+}
+
+func (set1 Set[K]) Union(set2 Set[K]) Set[K] {
+  res := make(Set[K])
+  for e := range set1 {
+    res.Add(e)
+  }
+
+  for e := range set2 {
+    res.Add(e)
+  }
+  return res
+}
+
+func (set1 Set[K]) Intersection(set2 Set[K]) Set[K] {
+  res := make(Set[K])
+  if set1.Size() > set2.Size() {
+    set1, set2 = set2, set1 // Swap to iterate over shorter set
+  }
+
+  for e := range set1 {
+    if set2.Has(e) {
+      res.Add(e)
+    }
+  }
+  return res
+}
+
+func (set1 Set[K]) Difference(set2 Set[K]) Set[K] {
+  res := make(Set[K])
+  for e := range set1 {
+    if !set2.Has(e) {
+      res.Add(e)
+    }
+  }
+  return res
+}
---
+++ ds/set_test.go
@@ -0,0 +1,210 @@
+package ds
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMakeSet(t *testing.T) {
+	set := MakeSet[int](1, 2, 3)
+	if len(set) != 3 {
+		t.Error("Expected 3 items in set. Received:", len(set))
+	}
+
+	for e := range set {
+		if e != 1 && e != 2 && e != 3 {
+			t.Error("Expected set to contain only 1, 2 and 3. Received:", e)
+		}
+	}
+}
+
+func slicesContentEqual[K comparable](a []K, b []K) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+outerLoop:
+	for _, v := range a {
+		for _, w := range b {
+			if v == w {
+				continue outerLoop
+			}
+		}
+		return false
+	}
+	return true
+}
+
+func TestSet_ToSlice(t *testing.T) {
+	set1 := MakeSet[int](1, 2, 3)
+	set2 := MakeSet[int](3, 4)
+
+	exp := []int{1, 2, 3}
+	res := set1.ToSlice()
+	if !slicesContentEqual(exp, res) {
+		t.Errorf("Expected converted slice to be equal: %v. Received: %v.", exp, res)
+	}
+
+	exp = []int{3, 4}
+	res = set2.ToSlice()
+	if !slicesContentEqual(exp, res) {
+		t.Errorf("Expected converted slice to be equal: %v. Received: %v.", exp, res)
+	}
+}
+
+func ExampleSet_String() {
+	set := MakeSet[int]()
+	fmt.Println(set)
+	set = MakeSet[int](1)
+	fmt.Println(set)
+
+	// Output:
+	// set[]
+	// set[1]
+}
+
+func TestSet_Has(t *testing.T) {
+	set := MakeSet[int](1, 2, 3)
+
+	res := set.Has(2)
+	if res == false {
+		t.Error("Expected true for set[1 2 3]. Received:", res)
+	}
+
+	res = set.Has(9)
+	if res == true {
+		t.Error("Expected false for set[1 2 3]. Received:", res)
+	}
+}
+
+func TestSet_Equal(t *testing.T) {
+	set1 := MakeSet[int](1, 2, 3)
+	set2 := MakeSet[int](1, 2, 3)
+	if !set1.Equal(set2) {
+		t.Error("Expected set1 to be equal to set2.")
+	}
+	set3 := MakeSet[int](1, 2)
+	if set1.Equal(set3) {
+		t.Error("Expected set1 not to be equal to set3.")
+	}
+}
+
+func TestSet_Add(t *testing.T) {
+	set := MakeSet[int]()
+	set.Add(1, 2, 3, 2, 1)
+
+	exp := MakeSet[int](1, 2, 3)
+	if !set.Equal(exp) {
+		t.Errorf("Expected set to be equal: %v. Received: %v.", exp, set)
+	}
+}
+
+func TestSet_Size(t *testing.T) {
+	set := MakeSet[int]()
+
+	exp := 0
+	res := set.Size()
+	if res != exp {
+		t.Errorf("Expected set of size %v. Received size: %v.", exp, res)
+	}
+
+	exp = 3
+	set.Add(1, 2, 3)
+	res = set.Size()
+	if res != exp {
+		t.Errorf("Expected set of size %v. Received size: %v.", exp, res)
+	}
+}
+
+func TestSet_Delete(t *testing.T) {
+	set := MakeSet[int](1, 2, 3)
+
+	set.Delete(2)
+	if set.Has(2) {
+		t.Error("Expected 2 not to be in set.")
+	}
+
+	set.Delete(8)
+	if set.Has(8) {
+		t.Error("Expected 8 not to be in set.")
+	}
+}
+
+func TestSet_Clear(t *testing.T) {
+	set := MakeSet[int](1, 2, 3)
+
+	set.Clear()
+	size := set.Size()
+	if size != 0 {
+		t.Error("Expected empty set. Received set of size:", size)
+	}
+}
+
+func TestSet_Copy(t *testing.T) {
+	set := MakeSet[int](1, 2, 3)
+
+	exp := MakeSet[int](1, 2, 3)
+	res := set.Copy()
+	if !res.Equal(exp) {
+		t.Errorf("Expected copied set to be equal: %v. Received: %v.", exp, res)
+	}
+
+	set.Delete(2)
+	if !res.Equal(exp) {
+		t.Errorf("Expected copied set to be equal: %v. Received: %v.", exp, res)
+	}
+}
+
+func TestSet_Union(t *testing.T) {
+	set1 := MakeSet[int](1, 2, 3)
+	set2 := MakeSet[int](2, 3, 4)
+	emptySet := MakeSet[int]()
+
+	exp := MakeSet[int](1, 2, 3, 4)
+	res := set1.Union(set2)
+	if !res.Equal(exp) {
+		t.Errorf("Expected union to be equal: %v. Received: %v.", exp, res)
+	}
+
+	exp = set1
+	res = set1.Union(emptySet)
+	if !res.Equal(exp) {
+		t.Errorf("Expected union to be equal: %v. Received: %v.", exp, res)
+	}
+}
+
+func TestSet_Intersection(t *testing.T) {
+	set1 := MakeSet[int](1, 2, 3)
+	set2 := MakeSet[int](2, 3, 4)
+	emptySet := MakeSet[int]()
+
+	exp := MakeSet[int](2, 3)
+	res := set1.Intersection(set2)
+	if !res.Equal(exp) {
+		t.Errorf("Expected intersection to be equal: %v. Received: %v.", exp, res)
+	}
+
+	exp = emptySet
+	res = set1.Intersection(emptySet)
+	if !res.Equal(exp) {
+		t.Errorf("Expected intersection to be equal: %v. Received: %v.", exp, res)
+	}
+}
+
+func TestSet_Difference(t *testing.T) {
+	set1 := MakeSet[int](1, 2, 3)
+	set2 := MakeSet[int](2, 3, 4)
+	emptySet := MakeSet[int]()
+
+	exp := MakeSet[int](1)
+	res := set1.Difference(set2)
+	if !res.Equal(exp) {
+		t.Errorf("Expected difference to be equal: %v. Received: %v.", exp, res)
+	}
+
+	exp = set1
+	res = set1.Difference(emptySet)
+	if !res.Equal(exp) {
+		t.Errorf("Expected difference to be equal: %v. Received: %v.", exp, res)
+	}
+}
